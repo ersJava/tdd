b88b56434025709d67098c7b33f28c0c92fee61e
Beth <ersergeant@gmail.com>
2019-11-18 10:26:46 -0500

    csv file source test example

--- testing java with junit/src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ testing java with junit/src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -4,6 +4,9 @@ import guru.springframework.sfgpetclinic.ModelTests;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -32,10 +35,39 @@ class OwnerTest implements ModelTests {
         // Hamcrest example
         assertThat(owner.getCity(), is("Denver"));
     }
+
     @DisplayName("Value Source Test")
     @ParameterizedTest(name ="{displayName} - [{index}] {arguments}")
     @ValueSource(strings = {"Spring", "Framework", "Guru"})
     void testValueSource(String val) {
         System.out.println("value: " + val);
+    }
+
+    @DisplayName("Enum Source Test")
+    @ParameterizedTest(name ="{displayName} - [{index}] {arguments}")
+    @EnumSource(OwnerType.class)
+    void enumTest(OwnerType ownerType) {
+        System.out.println("Owner type: " + ownerType);
+    }
+
+    @DisplayName("CSV Input Test")
+    @ParameterizedTest(name ="{displayName} - [{index}] {arguments}")
+    @CsvSource({
+            "CO, 1, 1",
+            "WA, 2, 2",
+            "MA, 3, 3",
+
+    })
+    void csvInputTest(String stateName, int value1, int value2) {
+        System.out.println(stateName + " = " + value1 + ":" + value2);
+
+    }
+
+    @DisplayName("CSV From File Test")
+    @ParameterizedTest(name ="{displayName} - [{index}] {arguments}")
+   @CsvFileSource(resources = "/input.csv", numLinesToSkip = 1)
+    void csvFromFileTest(String stateName, int value1, int value2) {
+        System.out.println(stateName + " = " + value1 + ":" + value2);
+
     }
 }
